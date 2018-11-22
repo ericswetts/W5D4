9b9c151080e6c9ac316c8ba9656ac9cb1f361edc
ericswetts <eric.swetts@gmail.com>
2018-11-22 15:57:02 -0800

    Implement #myTranspose for phase1_arrays

--- Javascript intro/phase_1_arrays.js
+++ Javascript intro/phase_1_arrays.js
@@ -9,9 +9,9 @@ Array.prototype.uniq = function()  {
     }
   }
   return result;
-}
+};
 
-
+// Two Sum
 Array.prototype.twoSum = function() {
   let result = [];
   let i;
@@ -19,14 +19,44 @@ Array.prototype.twoSum = function() {
     let j;
     for (j = i + 1; j < this.length; j++) {
       if (this[i] + this[j] === 0) {
-        console.log(`i = ${i}`);
-        console.log(`j = ${j}`);
+
         let pair = [i,j];
-        console.log(`pair = ${pair}`);
         result.push(pair);
       }
     }
   }
   return result;
-}
-// Two Sum
+};
+
+// // Transpose
+// Array.prototype.myTranspose = function() {
+//   // debugger;
+//   let n;
+//   let result = new Array(this[0].length);
+//   for (n = 0; n < this.length; n++) {
+//     result.push(new Array(this.length).fill(null));
+//   }
+//   let i;
+//   for (i = 0; i < this.length; i++) {
+//     let j;
+//     for (j = 0; j < this[0].length; j++) {
+//       result[i][j] = this[j][i];
+//     }
+//   }
+//   return result;
+// };
+// [[1,2,3],[4,5,6]].myTranspose();
+
+
+Array.prototype.myTranspose = function() {
+  let result = new Array(this[0].length).fill([]);
+  let row;
+  for (row = 0; row < this.length; row++) {
+    let col;
+    for (col = 0; col < this[0].length; col++) {
+      debugger;
+      result[col].push(this[col][row]);
+    }
+  }
+  return result;
+};
